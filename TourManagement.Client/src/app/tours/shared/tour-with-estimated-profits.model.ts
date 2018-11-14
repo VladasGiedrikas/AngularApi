f78200527b071ff684bbd4de36d10e4b656e29b5
VGiedrikas <vgiedrikas@gmail.com>
2018-11-14 17:27:16 +0200

    first comit

---
+++ TourManagement.Client/src/app/tours/shared/tour-with-estimated-profits.model.ts
@@ -0,0 +1,4 @@
+import { Tour } from "./tour.model";
+export class TourWithEstimatedProfits extends Tour {
+    estimatedProfits: number;
+}
---
+++ TourManagement.Client/src/app/tours/shows/shows.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Show } from './shared/show.model';
+
+@Component({
+  selector: 'app-shows',
+  templateUrl: './shows.component.html',
+  styleUrls: ['./shows.component.css']
+})
+
+export class ShowsComponent implements OnInit {
+
+  @Input() shows: Show[];
+
+  ngOnInit() {
+
+  }
+
+}
